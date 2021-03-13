8c557450d363f6974c70dd9bb825da0820c45ffd
Michael Fuget <michaelfuget@gmail.com>
2021-03-13 21:27:53 +0000

    Automatically backed up by Learn

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,19 @@
 // Code Keypad Component Here
+export default class Keypad extends Component {
+  handleKeyUp = () =>
+  {
+    console.log('Entering password...')
+  }
+  handleFocus = () =>
+  {
+    console.log('Good!')
+  }
+  render(){
+    return (
+      <input type = "password"
+        onblur = {handleBlur}
+        onfocus = {handleFocus}
+        ></input>
+    )
+  }
+}
